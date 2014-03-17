e04f1364bb039bd0f9391261d12696c1ae598806
magnushg <magnus.h.green@gmail.com>
2014-03-17 19:39:30 +0100

    Demo baseline with basic setup of arduino server app

--- server.js
+++ server.js
@@ -21,31 +21,10 @@ board.on("ready", function() {
   var thermSensor = five.Sensor({pin: "I0", freq: moment.duration(30, 'seconds').asMilliseconds()});
   var photoSensor = five.Sensor({pin: "I2", freq: moment.duration(30, 'seconds').asMilliseconds()});
 
-  thermSensor.on("data", function() {
-     var tempCelsius = converter.celsius(this.value).toFixed(1)
-     automatrFirebase.update({temprature: {value:tempCelsius, timestamp:Date.now()}});     
-     environmentLog.push({temprature: {value: tempCelsius, timestamp: Date.now()}});
-     //lcd.cursor(0, 0).print("Temp: " + converter.celsius(this.value).toFixed(1) + String.fromCharCode(223) + "C");
-  });
-
-  photoSensor.on("data", function () {
-    automatrFirebase.update({brightness: {value: this.value, timestamp: Date.now()}});
-    environmentLog.push({brightness: {value: this.value, timestamp: Date.now()}});
-  })
-
-  automatrFirebase.on('value', function (snapshot) {
-        snapshot.val().lightswitch ? led.on() : led.off();        
-    });
-
   board.repl.inject({
     led: led,
     thermSensor: thermSensor
-  }); 
-
-});
-
-app.get('/message', function(req, res) {	
-	res.json({message:'ok'});
+  });
 });
 
 app.listen(port);
