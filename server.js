873269a25f2a7f8dc637c61787e237d4f58aa552
magnushg <magnus.h.green@gmail.com>
2014-05-23 14:10:09 +0200

    Ever more stripped down starter solution, only contains barebones node server

--- server.js
+++ server.js
@@ -1,24 +1,7 @@
 var express = require("express"),
     five = require("johnny-five"),
-    Firebase = require("firebase"),
-    converter = require("./utils/converter"),
-    stringUtils = require("./utils/stringUtils"),
   	app = express(),
-	  port = process.env.PORT || 9900,
-    firebaseName = 'automatr-test';
-
-var automatrFirebase = new Firebase('https://{0}.firebaseio.com/'.format(firebaseName));
-var environmentLog = new Firebase('https://{0}.firebaseio.com/environmentLog'.format(firebaseName));
-
-var board = new five.Board();
-
-board.on("ready", function() {
- 
- board.repl.inject({
+	  port = process.env.PORT || 9900;
     
-  });
-
-});
-
 app.listen(port);
 console.log("Server started on port " + port);
