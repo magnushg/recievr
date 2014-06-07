ce24cfccfe7b299662db80fde319a45cfd099593
magnushg <magnus.h.green@gmail.com>
2014-06-07 19:53:00 +0200

    Code presented in NDC 2014 demo

--- server.js
+++ server.js
@@ -1,55 +1,71 @@
 var express = require("express"),
     five = require("johnny-five"),
-    Firebase = require("firebase"),
+    app = express(),
+    port = process.env.PORT || 9900,
     converter = require("./utils/converter"),
     stringUtils = require("./utils/stringUtils"),
-    moment = require("moment"),
+    Firebase = require("firebase"),
+    firebaseName = 'automatr-test',
+      
+      
+    automatrFirebase = new Firebase('https://{0}.firebaseio.com/'.format(firebaseName)),
+    environmentLog = new Firebase('https://{0}.firebaseio.com/environmentLog'.format(firebaseName));
+    
+    var board = new five.Board();
+    
+    board.on("ready", function() {
+     var led = five.Led("O0");
+          
+     var tempSensor = five.Sensor({pin: "I0", freq: 2000});
+     
+     tempSensor.on("data", function() {
+       var tempCelsius = converter.tempC(this.value).toFixed(1);
+       var temperature = {temperature: {value:tempCelsius, timestamp: Date.now()}};
+       automatrFirebase.update(temperature);    
+       environmentLog.push(temperature);
+       });
 
-	app = express(),
-	port = process.env.PORT || 9900,
-    Thermistor
-    firebaseName = 'automatr-test';
+     var coffeeMaker = new five.Relay("O3");
+     
+     automatrFirebase.on('value', function (snapshot) {
+           snapshot.val().coffee ? coffeeMaker.on() : coffeeMaker.off();
+       });
+     
+     var lightSensor = five.Sensor({pin: "I2", freq: 2000});
+     var lightSwitch = new five.Relay("O5");
+     
+     lightSensor.on("data", function () {
+         var brightness = {brightness: {value: this.value, timestamp: Date.now()}}
+         automatrFirebase.update(brightness);
+         environmentLog.push(brightness); 
+         this.value < 200 ? lightSwitch.on() : lightSwitch.off();
+         });
+     
+     var proximity = new five.Sensor({pin: "I5"});
+      
+     proximity.on("change", function() {
+       if(this.value > 0) {
+       automatrFirebase.update({proximity: {value: true, timestamp: Date.now()}});
+       led.on();
+       }
+       else {
+       automatrFirebase.update({proximity: {value: false, timestamp: Date.now()}});
+       led.off();
+       }
+     });
 
-var automatrFirebase = new Firebase('https://{0}.firebaseio.com/'.format(firebaseName));
-var environmentLog = new Firebase('https://{0}.firebaseio.com/environmentLog'.format(firebaseName));
 
-var board = new five.Board({port: "/dev/ttyAMA0"});
-
-board.on("ready", function() {
-  
-  var led = new five.Led("O5");
-  var thermSensor = five.Sensor({pin: "I0", freq: moment.duration(30, 'seconds').asMilliseconds()});
-  var photoSensor = five.Sensor({pin: "I2", freq: moment.duration(30, 'seconds').asMilliseconds()});
-  var proximity = new five.Sensor({pin: "I5"}); //freq: moment.duration(500, 'milliseconds').asMilliseconds()}
-
-  thermSensor.on("data", function() {
-     var tempCelsius = converter.celsius(this.value).toFixed(1)
-     automatrFirebase.update({temperature: {value:tempCelsius, timestamp: Date.now()}});    
-     environmentLog.push({temperature: {value: tempCelsius, timestamp: Date.now()}});
-     //lcd.cursor(0, 0).print("Temp: " + converter.celsius(this.value).toFixed(1) + String.fromCharCode(223) + "C");
-  });
-
-  photoSensor.on("data", function () {
-    automatrFirebase.update({brightness: {value: this.value, timestamp: Date.now()}});
-    environmentLog.push({brightness: {value: this.value, timestamp: Date.now()}});
-  });
-
-  proximity.on("change", function() {
-   //console.log(this.value);
-   this.value > 0 ? automatrFirebase.update({proximity: {value: true, timestamp: Date.now()}}) : automatrFirebase.update({proximity: {value: false, timestamp: Date.now()}});
-  });
-
-  automatrFirebase.on('value', function (snapshot) {
-        snapshot.val().lightswitch ? led.on() : led.off();      
+     automatrFirebase.on('value', function (snapshot) {
+       snapshot.val().lightswitch ? lightSwitch.on() : lightSwitch.off();   
+     });
+     
+       
+     board.repl.inject({
+        
+      });
+     
     });
-
-  board.repl.inject({
-    led: led,
-    thermSensor: thermSensor,
-    proximity: proximity
-  }); 
-
-});
-
+    
+    
 app.listen(port);
 console.log("Server started on port " + port);
